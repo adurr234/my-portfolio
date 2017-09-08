f46476dedf32e342699816ddf25885f1144aa2e4
Alexander Durr <30958138+adurr234@users.noreply.github.com>
2017-09-08 17:28:37 -0400

    Update portfolio deployment function for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -3,20 +3,24 @@ import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
 
 # source and destination buckets
-portfolio_bucket = s3.Bucket('portfolio.alexdurr.com')
-build_bucket = s3.Bucket('portfoliobuild.alexdurr.com')
+    portfolio_bucket = s3.Bucket('portfolio.alexdurr.com')
+    build_bucket = s3.Bucket('portfoliobuild.alexdurr.com')
 
 # for in memory unzipping
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
 # access zip output from Code Build and extract to S3
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm,
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    print("done")
+
+    return 'Hello from Lambda'
