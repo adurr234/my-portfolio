17038f1d683d2d277658aba5eee836d9be858a00
Alexander Durr <30958138+adurr234@users.noreply.github.com>
2017-09-08 21:00:00 -0400

    Use bucket name and object key from codepipeline, and notify when codepipline job done.

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,19 +6,28 @@ import mimetypes
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:515556474368:deployPortfolioTopic')
-
+    location = {
+        "bucketName": 'portfoliobuild.alexdurr.com',
+        "objectKey": 'portfoliobuild.zip'
+    }
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+
+        print("Building portfolio from " + str(location))
+
         s3 = boto3.resource('s3')
 
-    # source and destination buckets
         portfolio_bucket = s3.Bucket('portfolio.alexdurr.com')
-        build_bucket = s3.Bucket('portfoliobuild.alexdurr.com')
+        build_bucket = s3.Bucket(location["bucketName"])
 
-    # for in memory unzipping
         portfolio_zip = io.BytesIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
-    # access zip output from Code Build and extract to S3
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
@@ -27,6 +36,10 @@ def lambda_handler(event, context):
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
         print("done")
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully.")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+
     except:
         topic.publish(Subject="Portfolio Deploy Failed", Message="The Portfolio was not deployed successfully.")
         raise
