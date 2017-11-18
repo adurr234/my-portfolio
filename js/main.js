e51a858ac1555f5101b00c6dcf6d28b4a624fbf7
adurr234 <bob35354@yahoo.com>
2017-11-17 23:42:53 -0500

    Added Chatbot
    Added Chatbot as link on mainpage. Also updated thumbnail and description of example 1.

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ import ExampleWork from './example-work.js';
 
 const myWork = [
   {
-    'title': "Work Example",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'title': "A Chatbot",
+    'href': "https://d1wnhq2wvl9n5a.cloudfront.net/client.html",
+    'desc': "A chatbot using multiple AWS services. It can tell you more information about it's creation or about Chuck Norris",
     'image': {
-      'desc': "example screenshot",
-      'src': "images/example1.png",
+      'desc': "A Chatbot",
+      'src': "images/chatbot.png",
       'comment': ""
     }
   },
